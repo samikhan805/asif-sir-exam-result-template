9337de94039c544dd7cc0b9dd8384526367c5f4a
Sami Khan <76680018+samikhan805@users.noreply.github.com>
2021-07-20 23:45:28 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,742 @@
+var students = [{
+        name: 'Abdur Rahman Rafed',
+        college: 'Notre Dame College',
+        roll: 'SH105',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Sadekun Nur Ashekin Orpa',
+        college: 'Viqarunnisa Noon School and College',
+        roll: 'S105',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Ummy Farhana Usni',
+        college: 'Savar Cantonment Public School and College',
+        roll: 'M139',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Akilur Rahman',
+        college: 'Notre Dame College',
+        roll: 'N104',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Nishat tamanna',
+        college: 'Rajuk uttara model college',
+        roll: 'R101',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Shanjida Gazi',
+        college: 'Ideal School and College',
+        roll: 'M138',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Nazifa Alamgir Tasneem ',
+        college: 'Ideal School and College',
+        roll: 'E117',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Fabiha Binte Mohsin',
+        college: 'Viqarunnisa Noon School & College',
+        roll: 'SP101',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Asif Mahmud',
+        college: 'Notre Dame College',
+        roll: 'N108',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Shakhawat Hossain Sabbir',
+        college: 'Rajuk uttara model college',
+        roll: 'R127',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Anjila Binta Azad',
+        college: 'Rajuk uttara model college',
+        roll: 'R118',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Farjana Akter',
+        college: 'Ideal School and College',
+        roll: 'E182',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Raisha Anjum Risha',
+        college: 'Ideal School and College',
+        roll: 'E112',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Farjana Akter Emu',
+        college: 'Rajuk uttara model college',
+        roll: 'R104',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Raisa Akter',
+        college: 'Motijheel Model College',
+        roll: 'E135',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Sarwar Hossain Khan',
+        college: 'Notre Dame College',
+        roll: 'SH107',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Tahsina Marjan',
+        college: 'Gazipur Govt. Mohila College',
+        roll: 'M102',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Saima Akther Sumona',
+        college: 'Holy Cross College',
+        roll: 'B310',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Nusrat Jahan Mou',
+        college: 'Rajuk uttara model college',
+        roll: 'R107',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Prionty Roy',
+        college: 'Ideal School and College',
+        roll: 'T273',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Ahnaf Muttaki Shifat',
+        college: 'Notre Dame College',
+        roll: 'N119',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Mehedi Hasan',
+        college: 'Notre Dame College',
+        roll: 'N116',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Mahbuba Mojib Surayea',
+        college: 'Viqarunnisa Noon College',
+        roll: 'M143',
+        score: '',
+        position: ''
+    },
+    // {
+    //     name: 'Akhter Ahsanul Azim',
+    //     college: '',
+    //     roll: '',
+    //     score: '',
+    //     position: ''
+    // },
+    {
+        name: 'Israt Zahan Ema',
+        college: 'Ideal School and College',
+        roll: 'T207',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Shajnin Shabnam Joyeeta',
+        college: 'Viqarunnisa Noon College',
+        roll: 'S105',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Iffat Anan',
+        college: 'Notre Dame College',
+        roll: 'SH103',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Mosammot Somiya Akter',
+        college: 'Rajuk uttara model college',
+        roll: 'R137',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Farhana Sadia',
+        college: 'Ideal School and College',
+        roll: 'E113',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Sanjana Shamnur',
+        college: 'Viqarunnisa Noon College',
+        roll: 'E136',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Mohammad Shajid Ahsan Rafiu',
+        college: 'Notre Dame College',
+        roll: 'SH155',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Mahmudul Islam Sourov',
+        college: 'Notre Dame College',
+        roll: 'SH156',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Muhammad Nure Ferdous',
+        college: 'National Ideal College',
+        roll: 'N117',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Mobasshira Akter Mithila',
+        college: 'Ideal School and College',
+        roll: 'T271',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Mahmudul Hasan',
+        college: 'Dhaka College',
+        roll: 'C155',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Fatema Jannat Ruhi',
+        college: 'Dhaka City College',
+        roll: 'T266',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Suraiya Sultana',
+        college: 'Ideal School and College',
+        roll: 'B350',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Fahmida Habib Tultul',
+        college: 'Ideal School and College',
+        roll: 'M101',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Sanjida Alam Samia',
+        college: 'Rajud Uttara Model College',
+        roll: 'R117',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Mosharat Mehbuba Hossain Mridula',
+        college: 'Begum Badrunnessa Govt. Girls College',
+        roll: 'T246',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Masudur Rahman',
+        college: 'Rajuk Uttara Model College',
+        roll: 'R136',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Md. Osman Goni',
+        college: 'Rajuk Uttara Model College',
+        roll: 'R115',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Sakibun Islam Lamia',
+        college: 'Ideal School and College',
+        roll: 'E122',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Sadik Manower Bhuiyan',
+        college: 'Sabujhbagh Govt. College',
+        roll: 'N118',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Miftaha Nawar',
+        college: 'Ideal School and College',
+        roll: 'T265',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Fahim Ahmed',
+        college: 'Notre Dame College',
+        roll: 'N151',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Tasnim Chowdhury Rahi',
+        college: 'Ideal School and College',
+        roll: 'B326',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Juwairia Binte Ashraf',
+        college: 'Ideal School and College',
+        roll: 'E124',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Sumaiya Jannat',
+        college: 'Sabujbagh Govt. College',
+        roll: 'B322',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'latifa Nishat Nishi',
+        college: 'Ideal School and College',
+        roll: 'B302',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Abu Nayem',
+        college: 'Notre Dame College',
+        roll: 'N112',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Md. Thahamid All Mahabub Alin',
+        college: 'Notre Dame College',
+        roll: 'N105',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Maijuba Maisha',
+        college: 'Rajuk Uttara Model College',
+        roll: 'R108',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Suhana Tasneem',
+        college: 'Ideal School and College',
+        roll: 'E127',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Md. Mubtasim Rahman Dhrubo',
+        college: 'University Laboratory College',
+        roll: 'N150',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Atiqur Rahman Afif',
+        college: 'Notre Dame College',
+        roll: 'SH101',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Ayesha Siddiqa',
+        college: 'Begum Badrunnessa Govt. Girls College',
+        roll: 'M103',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Ahmed Shofi Ratul',
+        college: 'Notre Dame College',
+        roll: 'N103',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Zannatul Ferdous Nijhum',
+        college: 'Ideal School and College',
+        roll: 'T282',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Md. Eamin Rahman',
+        college: 'Rajuk Uttara Model College',
+        roll: 'R106',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Md. Sajjad Sarker',
+        college: 'Rajuk Uttara Model College',
+        roll: 'R126',
+        score: '',
+        position: ''
+    },
+    {
+        name: 'Samiha Tasnim Raisa',
+        college: 'Viqarunnisa Noon School and College',
+        roll: 'SP100',
+        score: '',
+        position: '',
+        email: 'samiharaisa2003@gmail.com'
+    },
+    {
+        name: 'Shishir Roy',
+        college: 'Notre Dame College',
+        roll: 'N120',
+        score: '',
+        position: '',
+        email: 'royshishir906@gmail.com'
+    },
+    {
+        name: 'Anindita Acharjee',
+        college: 'Viqarunnisa Noon School And College',
+        roll: 'M132',
+        score: '',
+        position: '',
+        email: 'aninditaacharjee292@gmail.com'
+    },
+    {
+        name: 'Mst. Lamia',
+        college: 'Begum Badrunnessa Govt Girls College',
+        roll: 'M123',
+        score: '',
+        position: '',
+        email: 'lamiatabassum442@gmail.com'
+    },
+    {
+        name: 'Saidur Rahman',
+        college: 'Notre Dame College',
+        roll: 'N154',
+        score: '',
+        position: '',
+        email: 'saidurrahman2357@gmail.com'
+    },
+    {
+        name: 'Md.Muzahidul Islam Alif',
+        college: 'Dhaka City College',
+        roll: 'SP150',
+        score: '',
+        position: '',
+        email: 'islamalif519@gmail.com'
+    },
+    {
+        name: 'Md Shadman Alam',
+        college: 'Notre Dame College',
+        roll: 'SH118',
+        score: '',
+        position: '',
+        email: 'alamshadman782@gmail.com'
+    },
+    {
+        name: 'Md. Moshiur Rahman',
+        college: 'Notre Dame College',
+        roll: 'N110',
+        score: '',
+        position: '',
+        email: 'hmovi2002@gmail.com'
+    },
+    {
+        name: 'Ashraful Islam Mahfuz',
+        college: 'Notre Dame College',
+        roll: 'N115',
+        score: '',
+        position: '',
+        email: 'ashrafulmahfuz4@gmail.com'
+    },
+    {
+        name: 'Nishad Tasnim',
+        college: 'Ideal School and College',
+        roll: 'B332',
+        score: '',
+        position: '',
+        email: ''
+    },
+    {
+        name: 'Sabah',
+        college: 'Viqarunnisa Noon School and College',
+        roll: 'M130',
+        score: '',
+        position: '',
+        email: ''
+    },
+    {
+        name: 'Abida Sultana Antora',
+        college: 'Ideal School and College',
+        roll: 'T172',
+        score: '',
+        position: '',
+        email: ''
+    },
+    {
+        name: 'Fabliha Nawal Maira',
+        college: 'National Ideal College',
+        roll: 'E173',
+        score: '',
+        position: '',
+        email: ''
+    },
+    {
+        name: 'Rimi Sarker',
+        college: 'Viqarunnisa Noon School and College',
+        roll: 'T247',
+        score: '',
+        position: '',
+        email: ''
+    },
+    {
+        name: 'Mahin',
+        college: 'Notre Dame College',
+        roll: 'SH157',
+        score: '',
+        position: '',
+        email: ''
+    },
+    {
+        name: 'Dhruba Das Messi',
+        college: 'Saint Joseph Higher Secondary School',
+        roll: 'B356',
+        score: '',
+        position: '',
+        email: ''
+    },
+    {
+        name: 'Tahia Rashid',
+        college: 'Ideal School and College',
+        roll: 'T279',
+        score: '',
+        position: '',
+        email: ''
+    },
+    {
+        name: 'Arup Kumar Das',
+        college: 'Notre Dame College',
+        roll: 'N107',
+        score: '',
+        position: '',
+        email: ''
+    },
+    {
+        name: 'Shafiuddin',
+        college: 'National Ideal College',
+        roll: 'SH108',
+        score: '',
+        position: '',
+        email: ''
+    },
+    {
+        name: 'Junayed Islam',
+        college: 'Notre Dame College',
+        roll: 'N111',
+        score: '',
+        position: '',
+        email: ''
+    },
+    {
+        name: 'Mahim Khan',
+        college: 'Notre Dame College',
+        roll: 'N106',
+        score: '',
+        position: '',
+        email: ''
+    },
+    {
+        name: 'Rihat',
+        college: 'Rajuk Uttara Model College',
+        roll: 'R138',
+        score: '',
+        position: '',
+        email: ''
+    },
+    {
+        name: 'Sarah Hosne Jahan',
+        college: 'Shaheed Bir Uttam Lt Anwar Girls\' College',
+        roll: 'M150',
+        score: '',
+        position: '',
+        email: ''
+    }
+    // {
+    //     name: '',
+    //     college: '',
+    //     roll: '',
+    //     score: '',
+    //     position: '',
+    //     email: ''
+    // }
+]
+
+var input = document.querySelector('.roll')
+var btn = document.querySelector('button')
+var content = document.querySelector('.content')
+var isFound = false
+var refresh = document.querySelector('.refresh')
+var sumResult = document.querySelector('.sum-res')
+var Print = document.querySelector('.print')
+var container = document.querySelector('.container')
+var notiDiv = document.querySelector('.notification-div')
+var close = document.querySelector('.close')
+
+var num1 = document.querySelector('.num-1')
+var num2 = document.querySelector('.num-2')
+
+var ran1 = Math.floor(Math.random() * 10) + 1
+var ran2 = Math.floor(Math.random() * 10) + 1
+
+num1.innerHTML = ran1
+num2.innerHTML = ran2
+
+function main() {
+    for (var i = 0; i < students.length; i++) {
+        // if (input.value == '' && sumResult.value == '') {
+        //     alert('Please input the two fileds')
+        //     break
+        // } else if (input.value == '') {
+        //     alert('Please input your batch roll')
+        //     break
+        // } else if (sumResult.value == '') {
+        //     alert('Prove that you\'re not a robot by inputing the correct sum in the second field')
+        //     break
+        // }
+        if (input.value == students[i].roll && (ran1 + ran2 == Number(sumResult.value))) {
+            // Name
+            var name = document.createElement('p')
+            var nameText = document.createTextNode(`Name: ${students[i].name.toUpperCase()}`)
+            name.appendChild(nameText)
+            content.appendChild(name)
+                // College
+            var college = document.createElement('p')
+            var collegeText = document.createTextNode(`College: ${students[i].college}`)
+            college.appendChild(collegeText)
+            content.appendChild(college)
+                // Batch roll
+            var roll = document.createElement('p')
+            var rollText = document.createTextNode(`Batch roll: ${students[i].roll}`)
+            roll.appendChild(rollText)
+            content.appendChild(roll)
+
+            var score = document.createElement('p')
+            var scoreText = document.createTextNode(`Score: ${students[i].score}`)
+            score.appendChild(scoreText)
+            content.appendChild(score)
+
+            var position = document.createElement('p')
+            var positionText = document.createTextNode(`Position: ${students[i].position}`)
+            position.appendChild(positionText)
+            content.appendChild(position)
+
+            var email = document.createElement('p')
+            var emailText = document.createTextNode(`Email: ${students[i].email}`)
+            email.appendChild(emailText)
+            content.appendChild(email)
+
+            name.classList.add('content-style')
+            college.classList.add('content-style')
+            roll.classList.add('content-style')
+            score.classList.add('content-style')
+            position.classList.add('content-style')
+            email.classList.add('content-style')
+
+            var grade = document.createElement('p')
+            var gradeText
+
+            // Grading
+            if (Number(students[i].score) < 20) {
+                gradeText = document.createTextNode(`Grade: F`)
+                grade.appendChild(gradeText)
+                content.appendChild(grade)
+            }
+
+            refresh.style.display = 'inline'
+            Print.style.display = 'inline'
+
+            input.disabled = true
+            input.value = ''
+            btn.disabled = true
+            btn.style.background = '#967969'
+            isFound = true
+
+        }
+    }
+    if (!isFound) {
+        container.classList.add('active')
+        notiDiv.classList.add('active')
+
+        console.log(404)
+    }
+
+}
+
+btn.addEventListener('click', () => {
+    main()
+})
+
+refresh.addEventListener('click', () => {
+    window.location.reload()
+})
+Print.addEventListener('click', () => {
+    window.print()
+})
+close.addEventListener('click', () => {
+    container.classList.remove('active')
+    notiDiv.classList.remove('active')
+})
+
+window.addEventListener('keydown', (key) => {
+    if (key.keyCode == '13') {
+        main()
+    }
+})
